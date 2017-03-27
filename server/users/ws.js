a52cfb01a9db90109f57b403c14101dfa28ae85d
oklamos <devmac@DEVMACs-MacBook-Pro.local>
2017-03-28 01:25:58 +0300

    bypass exceptions.. server will stuck in case of errors

--- server/users/ws.js
+++ server/users/ws.js
@@ -7,15 +7,17 @@ class UsersWS {
                     .post('/users', this.add.bind(this));
   }
 
-  get(req, res) {
-    service.get().then((users) => res.json(users));
+  get(req, res, next) {
+    service.get().then((users) => res.json(users))
+    .catch(next);
   }
 
-  add(req, res) {
+  add(req, res, next) {
     service.add({
       name: req.body.name,
       pass: req.body.pass
-    }).then((user) => res.json(user));
+    }).then((user) => res.json(user))
+    .catch(next);
   }
 
   getRouter() {
