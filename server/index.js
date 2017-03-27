097778be0aae82b1ae5ee83ebd08a42ea59bb57b
oklamos <devmac@DEVMACs-MacBook-Pro.local>
2017-03-28 00:20:14 +0300

    configurable listening port

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ app.use(bodyParser.json());
 const usersWS = new UsersWS();
 app.use(usersWS.getRouter());
 
-app.listen(3000, () => {
-    console.log('Listening on port 3000');
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log('Listening on port ' + port);
 });
