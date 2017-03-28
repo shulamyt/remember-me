0dce89e351b662eeba9d2f73298c30321a9151ea
Noam Shay <noamshay83@gmail.com>
2017-03-28 17:28:28 +0300

    add messages table to drop script

--- server/utils/storage/drop_schema.js
+++ server/utils/storage/drop_schema.js
@@ -1,3 +1,4 @@
 const StorageUtils = require('./StorageUtils');
 StorageUtils.query('DROP TABLE IF EXISTS users');
 StorageUtils.query('DROP TABLE IF EXISTS notification_clients');
+StorageUtils.query('DROP TABLE IF EXISTS messages');
