7c137be09b341bd8edde079e49d527b80c3783e1
Noam Shay <noamshay83@gmail.com>
2017-03-28 12:33:37 +0300

    create table if not exists
    create table if not exists

--- server/users/storage/create_schema.js
+++ server/users/storage/create_schema.js
@@ -1,2 +1,3 @@
 const StorageUtils = require('./utils');
-StorageUtils.query('CREATE TABLE users(id SERIAL PRIMARY KEY, name VARCHAR(40) not null, pass VARCHAR(40) not null)');
+StorageUtils.query('CREATE TABLE IF NOT EXISTS users(id SERIAL PRIMARY KEY, name VARCHAR(40) not null, pass VARCHAR(40) not null)');
+StorageUtils.query('CREATE TABLE IF NOT EXISTS notification_clients(id SERIAL PRIMARY KEY, name VARCHAR(40) not null, token VARCHAR(512) not null)');
