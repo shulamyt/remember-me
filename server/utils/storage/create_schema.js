a356924abdd2222c775cd712858b626a7639e64a
leongo01 <leon.goldovsky@gmail.com>
2017-03-28 15:34:44 +0300

    create message table

--- server/utils/storage/create_schema.js
+++ server/utils/storage/create_schema.js
@@ -1,3 +1,4 @@
-const StorageUtils = require('./utils');
+const StorageUtils = require('./StorageUtils');
 StorageUtils.query('CREATE TABLE IF NOT EXISTS users(id SERIAL PRIMARY KEY, name VARCHAR(40) not null, pass VARCHAR(40) not null)');
 StorageUtils.query('CREATE TABLE IF NOT EXISTS notification_clients(id SERIAL PRIMARY KEY, name VARCHAR(40) not null, token VARCHAR(512) not null)');
+StorageUtils.query('CREATE TABLE IF NOT EXISTS messages(id SERIAL PRIMARY KEY, userid INTEGER not null, message VARCHAR(1024) not null)');
