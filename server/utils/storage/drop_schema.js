48db9ccb6f25c150f6a7dbac8445e69811778c63
khamro <khatib.amro1@gmail.com>
2017-03-28 17:49:27 +0300

    drop students

--- server/utils/storage/drop_schema.js
+++ server/utils/storage/drop_schema.js
@@ -2,3 +2,4 @@ const StorageUtils = require('./StorageUtils');
 StorageUtils.query('DROP TABLE IF EXISTS users');
 StorageUtils.query('DROP TABLE IF EXISTS notification_clients');
 StorageUtils.query('DROP TABLE IF EXISTS messages');
+StorageUtils.query('DROP TABLE IF EXISTS students');
