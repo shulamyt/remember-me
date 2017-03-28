772835bdd3689d358ceefdea3a8741ddfa9ea2fc
Shulamyt <shulamyt@gmail.com>
2017-03-28 16:30:15 +0300

    clean code

--- client/src/remember-me.js
+++ client/src/remember-me.js
@@ -1,35 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import * as RestService from './utilities/rest-utilities';
 import RememberMe from './rememberMe';
-
-// RestService.get('/teacher').then(function(allTeacher) {
-// 	console.log('get: teacher');
-// 	console.log(allTeacher);
-// });
-
-// var teacherID = 1111;
-// RestService.get('/teacher?id=' + teacherID).then(function(teacher) {
-// 	console.log('get: teacher with id = ' + teacherID);
-// 	console.log(teacher);
-// });
-
-// RestService.get('/child').then(function(allChildren) {
-// 	console.log('get: child');
-// 	console.log(allChildren);
-// });
-
-// var childID = 111;
-// RestService.get('/child?id=' + childID).then(function(child) {
-// 	console.log('get: child with id = ' + childID);
-// 	console.log(child);
-// });
-
-// var teacherID = 222;
-// RestService.get('/child?teacherID=' + teacherID).then(function(children) {
-// 	console.log('get: children by teacherID = ' + teacherID);
-// 	console.log(children);
-// });
 
 var render = function(){
 	ReactDOM.render(
