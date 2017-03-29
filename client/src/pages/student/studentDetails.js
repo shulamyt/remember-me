f3806cc392e634b4297d17c75627a90608534f2b
evyatarge <evyatarge@gmail.com>
2017-03-29 21:49:23 +0300

    css student details

--- client/src/pages/student/studentDetails.js
+++ client/src/pages/student/studentDetails.js
@@ -8,9 +8,9 @@ class StudentDetails extends React.Component{
 			<div className="student-details">
 				<div className="student-name ">{this.props.student.name}</div>
                 <img className="student-picture" src={this.props.student.image}/>
-                <div className="student-properties">גיל: {this.props.student.age}</div>
-                <div className="student-properties">תחביבים: {this.props.student.hobbies}</div>
-                <div className="student-properties">קצת עלי: {this.props.student.about}</div>
+                <div className="student-properties"><b>גיל: </b> {this.props.student.age}</div>
+                <div className="student-properties"><b>התחביבים שלי :</b> {this.props.student.hobbies}</div>
+                <div className="student-properties"><b>קצת עלי :</b> {this.props.student.about}</div>
 
 			</div>
 		);
