afb9f9207cb9ab0b5355f0b59b9f712d2b403325
Noam Shay <noamshay83@gmail.com>
2017-03-29 18:33:39 +0300

    fix schedualing

--- server/message/MessageService.js
+++ server/message/MessageService.js
@@ -6,21 +6,30 @@ class MessageService {
 
     add({userid, message}) {
         return new Promise((resolve, reject)=>{
-            Storage.add({userid, message}).then((messageRow)=>{
-                resolve(messageRow);
-            }).catch((message)=>{"error add message to storage " + message});
+            Storage.add({userid, message}).then((fetchedMessage)=>resolve(this._toMessage(fetchedMessage))).catch((message)=>{throw new Error("error add message to storage " + message)});
         });
     }
 
     get(id){
         return new Promise((resolve, reject)=>{
-            Storage.get(id).then((messageRow) => {
-                resolve({
-                            "message": messageRow.message,
-                            "sudentId": messageRow.userid
-                        });
-            });
+            Storage.get(id).then((fetchedMessage)=>resolve(this._toMessage(fetchedMessage)))
+                            .catch((message)=>{throw new Error("error get message wit id " + id)});
         });
+    }
+
+    _toMessage(fetchedMessage) {
+        let message = null;
+        try{
+            message = JSON.parse(fetchedMessage.message);
+        }
+        catch(error){
+            reject("failed to parse message data " + fetchedMessage.message);
+        }
+        return {
+                    message: message,
+                    userid: fetchedMessage.userid,
+                    id: fetchedMessage.id
+                };
     }
 
     update({messageid, message}) {
