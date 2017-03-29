6ffaef9f9bfeaa7f4e20174a980db5f23d799272
sophienave <ashersophie@gmail.com>
2017-03-29 17:59:46 +0300

    Student Message component

---
+++ client/src/pages/student/studentMessages.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './student-messages.scss';
+
+class StudentMessages extends React.Component{
+
+	render(){
+		return (
+			<div className="student-messages"/>
+		);
+	}
+}
+
+export default StudentMessages;
--- client/src/pages/student/studentPage.js
+++ client/src/pages/student/studentPage.js
@@ -2,9 +2,22 @@ import React from 'react';
 import './student-page.scss';
 import StudentDetails from './studentDetails.js';
 import CreateMessage from './createMessage.js';
+import StudentMessages from './studentMessages.js'
+import * as RestService from './../../utilities/rest-utilities';
 
 
 class StudentPage extends React.Component{
+
+	constructor(props){
+		super(props);
+		this.state = {
+			messages: []
+		}
+	}
+
+	componentDidMount(){
+		this.fetchMessages();
+	}
 
 	render(){
 		return (
@@ -12,9 +25,19 @@ class StudentPage extends React.Component{
 				<div className="logo"/>
                 <CreateMessage studentName={this.props.student.name}/>
 				<StudentDetails student={this.props.student}/>
+				<StudentMessages messages={this.state.messages}/>
 				<div className="back-to-list-button">לעבור לרשימת הילדים</div>
 			</div>
 		);
+	}
+
+	fetchMessages(){
+		let self = this;
+		RestService.get('/message?studentId=' + self.props.student.id).then(function(messages) {
+			console.log('get: messages by studentId = ' + self.props.student.id);
+			console.log(messages);
+			self.setState({messages});
+		});		
 	}
 }
 
