024e3479b51133e78e0a6be47b3b04b80850d145
sophienave <ashersophie@gmail.com>
2017-03-29 18:43:44 +0300

    React Slick

--- client/src/pages/student/studentMessages.js
+++ client/src/pages/student/studentMessages.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import './student-messages.scss';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 class StudentMessages extends React.Component{
 
 	render(){
+		if(this.props.messages.length == 0){
+			return <div/>;
+		}
+
+		let settings = {
+		      dots: true,
+		      infinite: true,
+		      speed: 500,
+		      slidesToShow: 2,
+		      slidesToScroll: 1
+		    };
+
 		return (
-			<div className="student-messages">
-				{this.getMessageBody()}
-			</div>
+			<Slider {...settings}>
+				{this.getMessagesBody()}
+			</Slider>
 		);
 	}
 
-	getMessageBody(){
+	getMessagesBody(){
 		let messages;
 		if(this.props.messages){
 			messages = this.props.messages.map((message)=>{
