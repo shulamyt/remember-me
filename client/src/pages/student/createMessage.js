f5d036f58589aa7c78e14cac8ce81ce727fb02f5
tziporan <tziporan@amdocs.com>
2017-03-29 23:08:14 +0300

    clean fields after create message

--- client/src/pages/student/createMessage.js
+++ client/src/pages/student/createMessage.js
@@ -21,24 +21,21 @@ class CreateMessage extends React.Component{
         return  <div>
                     <div className="my-message-title" onChange={this.onWhenToSendChanged.bind(this)}>מתי לשלוח
                         <label className="radio">
-                            <div className="radio-label">עכשיו</div>
-                            <input type="radio" value="now" name="timing" defaultChecked={this.state.whenToSend === "now"}/>            
+                            <input type="radio" value="now" name="timing" defaultChecked={this.state.whenToSend === "now"}/> עכשיו
                         </label>
                         <label className="radio">
-                            {this.state.whenToSend === "date" ? this.renderDateField() : null}
-                            <div className="radio-label">בחר לפי תאריך ושעה</div>
-                            <input type="radio"  value="date" name="timing" defaultChecked={this.state.whenToSend === "date"}/> 
+                            <input type="radio"  value="date" name="timing" defaultChecked={this.state.whenToSend === "date"}/> בחק לפי תאריך ושעה
                         </label>
                         <label className="radio">
-                            <div className="radio-label">בחר לפי תדירות</div>
-                            <input type="radio" value="frequency" name="timing" defaultChecked={this.state.whenToSend === "frequency"}/>
+                            <input type="radio" value="frequency" name="timing" defaultChecked={this.state.whenToSend === "frequency"}/> בחר לפי תדירות
                         </label>
                     </div>
+                    {this.state.whenToSend === "date" ? this.renderDateField() : null}
                 </div>
     }
 
     renderDateField(){
-        return <div className="date-field">
+        return <div>
                     <DateTime  />
                 </div>
     }
@@ -82,6 +79,10 @@ class CreateMessage extends React.Component{
                         schedule: {date: date}
                     }
         this.props.onCompleteClicked(message);
+        this.setState({whenToSend: "now"});
+        this.setState({message: ""});
+        this.setState({sender: ""});
+        this.setState({sendDate: null});
     }
 
 	render(){
